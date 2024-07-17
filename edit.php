33409559e2ce051c79b168395e82451d5cf71848
Desriasinaga04 <desria2003@gmail.com>
2024-07-16 20:54:26 -0700

    upload file

--- edit.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include 'koneksi.php';
-$id = $_GET['p'];
-mysqli_query($con,"UPDATE from produk where id='$id'");
-header("location: produk.php");
-?>
--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root","","kuis_prakweb");
-
-if(mysqli_connect_errno()){
-    echo"failed to connect to mysql:".mysqli_connect_error();
-    exit();
-}
-?>
